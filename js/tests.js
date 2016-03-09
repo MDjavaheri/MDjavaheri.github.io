6089885a60db1f6281f212cf24b33a8ef3ed828d
M Djavaheri <michael.djavaheri@mail.yu.edu>
2016-03-09 02:56:40 -0500

    fixed typo

--- js/tests.js
+++ js/tests.js
@@ -63,5 +63,5 @@ QUnit.test("computer move dependants", function(assert) {
     assert.deepEqual(computer.moveCount, {"rock": 5, "paper": 3, "scissors": 2}, "Computer Move Tally Successfully Updates");
     assert.deepEqual(computer.faveMove(), "paper", "Favorite Move Successful");
     controller.reset();
-    assert.deepEqual(computer.moveCount, {"rock": 0, "paper": 0, "scissors": 0}, "Computer Move Tally Successfully Resets"
+    assert.deepEqual(computer.moveCount, {"rock": 0, "paper": 0, "scissors": 0}, "Computer Move Tally Successfully Resets");
 });
