90c68a44096a04b8b2599855abf99466c41e3785
M Djavaheri <michael.djavaheri@mail.yu.edu>
2016-03-09 15:42:16 -0500

    add computer and game rule tests

--- js/tests.js
+++ js/tests.js
@@ -27,10 +27,14 @@
 
 //Controller
 //reset works 
+//setup
+var computer = overseasFactory();
+var controller = newController();
 var scoreboard = newScoreBoard();
-QUnit.module("scoreboard-specific");
-QUnit.test("tally up and reset", function(assert) {
-   assert.expect(5);
+
+QUnit.module("Scoreboard");
+QUnit.test("Score Tally Works", function(assert) {
+   assert.expect(4);
    assert.deepEqual(scoreboard.scores, {"wins":0, "losses":0, "ties":0}, "Initialization Successul");
    scoreboard.win();
    assert.deepEqual(scoreboard.scores, {"wins":1, "losses":0, "ties":0}, "Win Successul");
@@ -38,30 +42,138 @@ QUnit.test("tally up and reset", function(assert) {
    assert.deepEqual(scoreboard.scores, {"wins":1, "losses":1, "ties":0}, "Lose Successul");
    scoreboard.tie();
    assert.deepEqual(scoreboard.scores, {"wins":1, "losses":1, "ties":1}, "Tie Successul");
+}
+QUnit.test("Reset", function(assert) {
    scoreboard.reset();
    assert.deepEqual(scoreboard.scores, {"wins":0, "losses":0, "ties":0}, "Reset Successul");
 });
 
-var computer = overseasFactory();
-var controller = newController();
-QUnit.module("General")
-QUnit.test("computer move dependants", function(assert) {
+QUnit.module("Computer");
+QUnit.test("Last Move Defaults to Random", function(assert) {
     assert.expect(6);
-    assert.ok(computer.lastMove() !== undefined,  "Last Move defaults to random move")
-    controller.play("rock");
+    assert.ok(computer.lastMove() !== undefined,  "Last Move defaults to random move when there are no previous player moves")
+});
+QUnit.test("Last Move Counter Updates", function(assert) {
+    assert.expect(2);
     controller.play("rock");
     assert.deepEqual(computer.lastMoveCounter, "paper", "Last Move updates correctly")
     controller.play("rock");
-    controller.play("rock");
-    controller.play("rock");
-    controller.play("paper");
-    controller.play("paper");
     controller.play("paper");
     assert.deepEqual(computer.lastMoveCounter, "scissors", "Last Move updates correctly")
+});
+controller.reset();//reset
+QUnit.test("Move Tally Works", function(assert) {
+    controller.play("scissors");
+    controller.play("rock");
+    controller.play("rock");
+    controller.play("rock");
+    controller.play("paper");
+    controller.play("paper");
+    controller.play("scissors");
+    assert.deepEqual(computer.moveCount, {"rock": 2, "paper": 3, "scissors": 2}, "Computer Move Tally Successfully Updates");
+});
+QUnit.test("Most Popular Move", function(assert) {
+    assert.expect(2);
+    controller.play("rock");
+    controller.play("rock");
+    controller.play("rock");
+    controller.play("rock");
+    controller.play("rock");
+    controller.play("rock");
+    controller.play("rock");
+    controller.play("paper");
+    controller.play("paper");
     controller.play("scissors");
     controller.play("scissors");
-    assert.deepEqual(computer.moveCount, {"rock": 2, "paper": 5, "scissors": 3}, "Computer Move Tally Successfully Updates");
+    controller.play("scissors");
     assert.deepEqual(computer.faveMove(), "paper", "Favorite Move Successful");
+    controller.play("scissors");
+    controller.play("scissors");
+    controller.play("scissors");
+    controller.play("scissors");
+    controller.play("scissors");
+    controller.play("scissors");
+    controller.play("scissors");
+    controller.play("scissors");
+    assert.deepEqual(computer.faveMove(), "rock", "Favorite Move Successful");
+});
+QUnit.test("Reset", function(assert) {
+    assert.expect(2);
+    controller.play("rock");
+    controller.play("paper");
+    controller.play("scissors");
+    assert.deepEqual(computer.moveCount, {"rock": 1, "paper": 1, "scissors": 1}, "Computer Move Tally Successfully Resets");
     controller.reset();
     assert.deepEqual(computer.moveCount, {"rock": 0, "paper": 0, "scissors": 0}, "Computer Move Tally Successfully Resets");
+});
+controller.reset();//reset
+QUnit.test("New Strategy", function(assert) {
+    controller.newStrategy("Random"); 
+    controller.newStrategy("Last"); 
+    assert.deepEqual(computer.strategy, "last", "Controller successfully updates computer strategy");   
+});
+
+QUnit.module("Game Rules");//using last move to manipulate the computer
+QUnit.test("Rock beats Scissors", function(assert) {
+    controller.reset();//reset 
+    controller.newStrategy("last"); 
+    controller.play("paper"); 
+    controller.play("rock"); 
+    assert.deepEqual(scoreboard.scores, {"wins":1, "losses":0, "ties":0}, "Rock > Scissors");
+});
+QUnit.test("Rock loses to Paper", function(assert) {
+    controller.reset();//reset 
+    controller.newStrategy("last"); 
+    controller.play("rock"); 
+    controller.play("rock"); 
+    assert.deepEqual(scoreboard.scores, {"wins":0, "losses":1, "ties":0}, "Rock < Paper");
+});
+QUnit.test("Rock ties with Rock", function(assert) {
+    controller.reset();//reset 
+    controller.newStrategy("last"); 
+    controller.play("scissors"); 
+    controller.play("rock"); 
+    assert.deepEqual(scoreboard.scores, {"wins":0, "losses":0, "ties":1}, "Rock === Rock");
+});
+QUnit.test("Scissors beats Paper", function(assert) {
+    controller.reset();//reset 
+    controller.newStrategy("last"); 
+    controller.play("rock"); 
+    controller.play("scissors"); 
+    assert.deepEqual(scoreboard.scores, {"wins":1, "losses":0, "ties":0}, "Scissors > Paper");
+});
+QUnit.test("Scissors loses to Rock", function(assert) {
+    controller.reset();//reset 
+    controller.newStrategy("last");
+    controller.play("scissors"); 
+    controller.play("scissors"); 
+    assert.deepEqual(scoreboard.scores, {"wins":0, "losses":1, "ties":0}, "Scissors < Rock");
+});
+QUnit.test("Scissors ties with Scissors", function(assert) {
+    controller.reset();//reset 
+    controller.newStrategy("last"); 
+    controller.play("rock"); 
+    controller.play("scissors"); 
+    assert.deepEqual(scoreboard.scores, {"wins":0, "losses":0, "ties":1}, "Scissors === Scissors");    
+});
+QUnit.test("Paper beats Rock", function(assert) {
+    controller.reset();//reset 
+    controller.newStrategy("last"); 
+    controller.play("scissors"); 
+    controller.play("paper"); 
+    assert.deepEqual(scoreboard.scores, {"wins":1, "losses":0, "ties":0}, "Paper > Rock");    
+});
+QUnit.test("Paper loses to Scissors", function(assert) {
+    controller.reset();//reset 
+    controller.newStrategy("last"); 
+    controller.play("paper"); 
+    controller.play("paper"); 
+    assert.deepEqual(scoreboard.scores, {"wins":0, "losses":1, "ties":0}, "Paper < Scissors");    
+});
+QUnit.test("Paper ties with Paper", function(assert) {
+   controller.reset();//reset 
+    controller.newStrategy("last"); 
+    controller.play("rock"); 
+    controller.play("paper"); 
+    assert.deepEqual(scoreboard.scores, {"wins":0, "losses":0, "ties":1}, "Paper === Paper");     
 });
